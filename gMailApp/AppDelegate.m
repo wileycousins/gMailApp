12525e44ee8529133e17dba5304ca8ee41e7a6b1
H. Cole Wiley <hcwiley@gmail.com>
2014-06-13 14:31:35 -0500

    tab titles just show account name

--- gMailApp/AppDelegate.m
+++ gMailApp/AppDelegate.m
@@ -27,6 +27,7 @@
   [window setInitialFirstResponder:tabView];
   [[NSUserNotificationCenter defaultUserNotificationCenter] setDelegate:self];
   
+  // clear old notifications
   [[NSUserNotificationCenter defaultUserNotificationCenter] removeAllDeliveredNotifications];
   
   //register to listen for event
@@ -109,18 +110,19 @@
   return loaderView;
 }
 
-NSInteger unread = 0;
-- (void)webView:(WebView *)sender didReceiveTitle:(NSString *)title forFrame:(WebFrame *)frame
-{
+- (void)webView:(WebView *)sender didReceiveTitle:(NSString *)title forFrame:(WebFrame *)frame {
   for( NSTabViewItem *item in [tabView tabViewItems] ){
     if (frame == [[item view] mainFrame]) {
-      [item setLabel:title];
-//      NSLog(@"Title: %@", title);
+      NSString *tabTitle;
+      NSInteger start = [title rangeOfString:@"-"].location+1;
+      NSInteger end = [title rangeOfString:@"-" options:NSBackwardsSearch].location;
+      tabTitle = [title substringWithRange:NSMakeRange(start,end-start)];
+      [item setLabel:tabTitle];
+      NSInteger unread = 0;
       if( [title rangeOfString:@"("].location != NSNotFound ){
-        NSInteger start = [title rangeOfString:@"("].location+1;
-        NSInteger end = [title rangeOfString:@")"].location;
-        NSString *uString = [title substringWithRange:NSMakeRange(start,end-start)];
-        unread = [uString integerValue];
+        start = [title rangeOfString:@"("].location+1;
+        end = [title rangeOfString:@")"].location;
+        unread = [[title substringWithRange:NSMakeRange(start,end-start)] integerValue];
       } else {
         unread = 0;
       }
@@ -130,14 +132,8 @@ NSInteger unread = 0;
         [[[NSApplication sharedApplication] dockTile] setBadgeLabel:nil];
       } else {
         // set the notification center stuff
-        NSString *uString;
-        if( [title rangeOfString:@"@"].location != NSNotFound ){
-          NSInteger start = [title rangeOfString:@"-"].location+1;
-          NSInteger end = [title rangeOfString:@"-" options:NSBackwardsSearch].location;
-          uString = [title substringWithRange:NSMakeRange(start,end-start)];
-        }
         NSUserNotification *notification = [[NSUserNotification alloc] init];
-        notification.title = uString;
+        notification.title = tabTitle;
         WebView *webView = [[tabView selectedTabViewItem] view];
         NSString *msg = [webView stringByEvaluatingJavaScriptFromString: @"document.getElementsByClassName('zE')[0].getElementsByTagName('b')[0].innerText"];
         NSLog(@"msg: %@",msg);
@@ -151,9 +147,9 @@ NSInteger unread = 0;
         [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:notification];
       }
     }
-
   }
 }
+
 - (IBAction)addNewAccount:(id)sender {
   // load gmail
   NSInteger index = [[tabView tabViewItems] count];
